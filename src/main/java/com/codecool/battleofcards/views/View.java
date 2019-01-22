12266d68c101cc81b9a52f9ccee3aea2a64f124d
Kamil Bednarczyk <genezi333@poczta.fm>
2019-01-22 14:41:50 +0100

    Implement View

---
+++ src/main/java/com/codecool/battleofcards/views/View.java
@@ -0,0 +1,25 @@
+package com.codecool.battleofcards.views;
+
+import java.util.Scanner;
+
+abstract class View {
+
+    protected Scanner scanner = new Scanner(System.in);
+
+    public void print(String message) {
+        System.out.print(message);
+    }
+
+    public void println(String message) {
+        System.out.println(message);
+    }
+
+    public int getNumericInput() {
+        while (!scanner.hasNextInt()) {
+            println("Input must be an Integer. Try again.");
+        }
+        int number = scanner.nextInt();
+        scanner.nextLine();
+        return number;
+    }
+}
