0de01232ab7419242845778dc5bbf4774278bdbd
PatrykMandrak <patrykmandrak123@gmail.com>
2019-01-22 10:25:34 +0100

    Add database connector, CardDAO and CardDAO interface

---
+++ src/main/java/com/codecool/battleofcards/dao/ICardDAO.java
@@ -0,0 +1,14 @@
+package com.codecool.battleofcards.dao;
+
+import java.util.ArrayList;
+
+
+public interface ICardDAO {
+
+    //createASampleTable()
+    //insertNew(String name, int stat1, int stat2, int stat3 ...)
+    //update(int id)
+    //delete(int id)
+    //getAllCards()
+    //getCardById(int id)
+}
