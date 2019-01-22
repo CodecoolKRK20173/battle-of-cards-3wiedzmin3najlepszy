25d019280f75e550946070f46b193b02b0396f75
r4fgacon <r4fgacon@gmail.com>
2019-01-22 10:33:31 +0100

    table frame

---
+++ src/main/java/com/codecool/battleofcards/main/Table.java
@@ -0,0 +1,22 @@
+package com.codecool.battleofcards.main;
+
+public class Table {
+    private List<Player> = new ArrayList<Player>();;
+    Dealer dealer = new Dealer();
+    Deck deck = new Deck();
+
+    public int compareCards(int atrIndex) {
+        return 0;
+    }
+
+    public void addCardsToDeck(){}
+
+    public Player getCurrentPlayer(){
+        return null;
+    }
+
+    public boolean checkIfGameOn(Player player){
+        return true;
+    }
+
+}
