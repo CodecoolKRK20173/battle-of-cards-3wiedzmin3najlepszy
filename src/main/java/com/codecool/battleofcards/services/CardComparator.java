0c73f37f1560baba7bd4dec76dd9cc06fac2314b
Kamil Bednarczyk <genezi333@poczta.fm>
2019-01-22 11:06:47 +0100

    Implement card comparator

--- src/main/java/com/codecool/battleofcards/services/CardComparator.java
+++ src/main/java/com/codecool/battleofcards/services/CardComparator.java
@@ -1,4 +1,54 @@
 package com.codecool.battleofcards.services;
 
-public class CardComparator{
+import java.util.Comparator;
+
+public class CardComparator implements Comparator<Card> {
+
+    private AttributeType attributeType;
+
+    CardComparator(AttributeType attributeType) {
+        this.attributeType = attributeType;
+    }
+
+    @Override
+    public int compare(Card cardOne, Card cardTwo) {
+        int attrOne = getAttributeOf(cardOne);
+        int attrTwo = getAttributeOf(cardTwo);
+
+        if (attrOne > attrTwo) {
+            return 1;
+        } else if (attrOne == attrTwo) {
+            return 0;
+        } else {
+            return -1;
+        }
+    }
+
+    private int getAttributeOf(Card card) {
+        int attrValue;
+
+        switch (attributeType) {
+        case STRENGTH:
+            attrValue = card.getStrength();
+            break;
+        case MELEE:
+            attrValue = card.getMelee();
+            break;
+        case MAGIC:
+            attrValue = card.getMagic();
+            break;
+        case DEXTERITY:
+            attrValue = card.getDexterity();
+            break;
+        case INTELLIGENCE:
+            attrValue = card.getIntelligence();
+            break;
+        }
+
+        return attrValue;
+    }
+}
+
+enum AttributeType {
+    STRENGTH, MELEE, MAGIC, DEXTERITY, INTELLIGENCE
 }
