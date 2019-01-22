2b2766cb638a847d8923e9c6d55af294b2fc4e29
r4fgacon <r4fgacon@gmail.com>
2019-01-22 14:08:55 +0100

    table form

---
+++ src/main/java/com/codecool/battleofcards/controllers/TableController.java
@@ -0,0 +1,28 @@
+package com.codecool.battleofcards.controllers;
+
+import com.codecool.battleofcards.dao.CardDAO;
+import com.codecool.battleofcards.services.*;
+import com.codecool.battleofcards.views.EditorView;
+import com.codecool.battleofcards.views.GameView;
+
+public class TableController{
+    private Table table;
+    private GameView gameView;
+    private EditorView editorView;
+    private CardDAO cardDAO;
+
+    public void playGame(){
+
+    }
+
+    public void playRound(){
+        Table.checkIfGameOn();
+
+
+    }
+
+    public void initializeGame(){
+        table = new Table();
+        gameView = new GameView();
+    }
+}
--- src/main/java/com/codecool/battleofcards/services/Table.java
+++ src/main/java/com/codecool/battleofcards/services/Table.java
@@ -1,4 +1,37 @@
 package com.codecool.battleofcards.services;
 
+import com.codecool.battleofcards.dao.CardDAO;
+
+import java.util.*;
+
 public class Table{
+    private List<Player> players = new ArrayList<Player>();
+    Dealer dealer;
+    Deck deck;
+
+    public Table() {
+        dealer = new Dealer();
+        deck = new Deck();
+    }
+
+    public int compareCards(int atrIndex) {
+        return 0;
+    }
+
+    public Player getCurrentPlayer(){
+        for (Player player : players){
+            if (player.getActivePlayer())
+                return player;
+        }
+        return null;
+    }
+
+    public boolean checkIfGameOn(){
+        for (Player player : players) {
+            if (player.getCards().size() == 0)
+                return false;
+        }
+        return true;
+    }
+
 }
